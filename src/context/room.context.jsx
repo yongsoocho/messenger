ca4a6e8bbcbddabb2a08429246d4960b1363e506
yongsoocho <yongsoocho578@gmail.com>
2025-07-15 19:15:36 +0900

    socket 완성

--- src/context/room.context.jsx
+++ src/context/room.context.jsx
@@ -1,11 +1,46 @@
-import { createContext, useState } from "react";
+import { createContext, use, useEffect, useState } from "react";
 import axios from "axios";
+
+import { io } from "socket.io-client";
+const Socket = io("http://localhost:8080", { withCredentials: true });
 
 export const RoomContext = createContext();
 
 export const RoomProvider = ({ children }) => {
 	const [rooms, setRoom] = useState(null);
 	const [selectedUser, setSelectedUser] = useState(null);
+	const [messages, setMessages] = useState({});
+
+	// 방 가져오기
+	useEffect(() => {
+		fetchRooms();
+	}, []);
+
+	// room 리스트 가져오고 socket join
+	useEffect(() => {
+		if (rooms && rooms.length > 0) {
+			rooms.forEach((room) => {
+				Socket.emit("join", room.id);
+			});
+		}
+	}, [rooms]);
+
+	// 채팅방 메세지 가져오기
+	useEffect(() => {
+		if (!selectedUser || !Object.keys(selectedUser).length) return;
+
+		axios
+			.get(`http://localhost:8080/chat?roomId=${selectedUser?.id}`, {
+				withCredentials: true,
+			})
+			.then((res) => {
+				setMessages((prev) => ({
+					...prev,
+					[selectedUser.id]: res.data,
+				}));
+			});
+	}, [selectedUser]);
+	//
 
 	const fetchRooms = async () => {
 		try {
